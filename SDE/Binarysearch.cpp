c9303c8522bce64547c528b1472b1b7222e00f2d
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-03-06 00:29:15 +0530

    Create Binarysearch.cpp

---
+++ SDE/Binarysearch.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+      
+       int binarysearch(int arr[], int n, int k) {
+       
+        int l=0;
+        int h= n-1;
+         while(l<=h){
+            int mid=(l+h)/2;
+            if(arr[mid]==k){
+                return mid;
+            }
+            else if(arr[mid]<k){
+                l= mid+1;
+            }
+            else{
+                h= mid-1;
+            }
+           }
+           return -1;
+       }
+
+    
+    
+    
+};
