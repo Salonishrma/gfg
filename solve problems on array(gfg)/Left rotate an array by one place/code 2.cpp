151b2f83566d78fc3df3ef8dd75dcbc40a0a8edc
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-04-30 22:39:12 +0530

    Update code 2.cpp

--- solve problems on array(gfg)/Left rotate an array by one place/code 2.cpp
+++ solve problems on array(gfg)/Left rotate an array by one place/code 2.cpp
@@ -1,6 +1,45 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    void leftRotate(int arr[], int n, int d) {
+        // code here
+         reverse(arr,arr+d);
+         reverse(arr+d,arr+n);
+         reverse(arr,arr+n);
+      
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, d;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cin >> d;
+        Solution ob;
+        ob.leftRotate(arr, n, d);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
 }
+
+
+
+// } Driver Code Ends
