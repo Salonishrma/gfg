a180c4236fc1853f17b116562b17c756fc9e8485
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-04-30 23:08:42 +0530

    Update code 2.cpp

--- solve problems on array(gfg)/Searching an element in a sorted array/code 2.cpp
+++ solve problems on array(gfg)/Searching an element in a sorted array/code 2.cpp
@@ -1,6 +1,51 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-}
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find element in sorted array
+    // arr: input array
+    // N: size of array
+    // K: element to be searche
+    int searchInSorted(int arr[], int N, int K) 
+    { 
+    int ans=-1;
+       // Your code here
+       for(int i=0;i<N;i++){
+           if(arr[i]==K)
+           ans=1;
+       }
+       return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(void) 
+{ 
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.searchInSorted(arr, n, k) << endl;
+
+    }
+
+	return 0; 
+} 
+
+// } Driver Code Ends
