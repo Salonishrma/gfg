99d00366a5bdd685af949a56033d52f97e97d27d
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-04-30 23:17:47 +0530

    Update code 2.cpp

--- solve problems on array(gfg)/Union of Two Sorted Arrays/code 2.cpp
+++ solve problems on array(gfg)/Union of Two Sorted Arrays/code 2.cpp
@@ -1,6 +1,65 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //arr1,arr2 : the arrays
+    // n, m: size of arrays
+    //Function to return a list containing the union of the two arrays. 
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        //Your code here
+        //return vector with correct order of elements
+        unordered_set<int>s1;
+        for(int i=0;i<n;++i){
+            s1.insert(arr1[i]);
+        }
+         for(int i=0;i<m;++i){
+            s1.insert(arr2[i]);
+        }
+        vector<int>ans;
+        for(auto it:s1){
+            ans.push_back(it);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    
+	    
+	    
+	    int N, M;
+	    cin >>N >> M;
+	    
+	    int arr1[N];
+	    int arr2[M];
+	    
+	    for(int i = 0;i<N;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<M;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    vector<int> ans = ob.findUnion(arr1,arr2, N, M);
+	    for(int i: ans)cout<<i<<' ';
+	    cout << endl;
+	    
+	}
+	
+	return 0;
 }
+// } Driver Code Ends
