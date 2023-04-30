3d93aa0d6874cae00f18de5ec1619d7dcecac10b
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-04-30 22:03:41 +0530

    Update code 2.cpp

--- solve problems on array(gfg)/Second Largest/code 2.cpp
+++ solve problems on array(gfg)/Second Largest/code 2.cpp
@@ -1,6 +1,48 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    int x = -1;
+	    int y = -1;
+	    if(n==1 || n==0) return -1;
+	    
+	    for(int i=0; i<n; i++){
+	        x = max(x, arr[i]);
+	    }
+	    for(int i=0; i<n; i++){
+	        if(arr[i]<x && arr[i]>y){
+	            y = arr[i];
+	        }
+	    }
+	    return y;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.print2largest(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
 }
+
+// } Driver Code Ends
