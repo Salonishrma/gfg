c917b0387c5060a70abb501e7d3750b1755fb2a6
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-05-01 20:28:09 +0530

    Update code.cpp

--- solve problems on array(gfg)/Longest Subarray with given Sum K/code.cpp
+++ solve problems on array(gfg)/Longest Subarray with given Sum K/code.cpp
@@ -1,6 +1,52 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int lenOfLongSubarr(int A[],  int N, int K) 
+    { 
+   int left=0;
+   int right=0;
+   long long sum=A[0];
+   int maxlength=0;
+   while(right<N){
+       while(left<=right && sum>K) //array of sum ki value jb greater thi k ki value s 
+       // then hm subtract krenge array ke element ko left side se
+       sum=sum-A[left];
+       left++;
+   }
+   if(sum==K){
+       maxlength =max(maxlength,right-left+1);
+   }
+   right++;
+   if(right<N){
+       sum=sum+A[right];
+   }
+   return maxlength;
+    } 
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	//code
+	
+	int t;cin>>t;
+	while(t--)
+	{
+	    int n, k;
+	    cin>> n >> k;
+	    int a[n];
+	    
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution ob;
+	   cout << ob.lenOfLongSubarr(a, n , k)<< endl;
+	    
+	}
+	
+	return 0;
 }
