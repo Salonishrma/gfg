15da72fbd9183608533bad2d487f20fabc48a0c7
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-05-01 20:25:57 +0530

    Add files via upload

--- solve problems on array(gfg)/Longest Subarray with given Sum K/code 2.cpp
+++ solve problems on array(gfg)/Longest Subarray with given Sum K/code 2.cpp
@@ -1,53 +1,6 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-class Solution{
-    public:
-    int lenOfLongSubarr(int A[],  int N, int K) 
-  {
-      int maxlength=0;
-      int sum=0;
-      map<long long,int>m1;
-      for(int i=0;i<N;i++){
-          sum=sum+A[i];
-          if(sum==K)
-           maxlength=max(maxlength,i+1);
-           int rem=sum-K;
-           if(m1.find(rem)!=m1.end()){
-               int len=i-m1[rem];
-               maxlength=max(maxlength,len);
-           }
-           if(m1.find(sum)==m1.end()){
-               m1[sum]=i;
-           }
-      }
-      return maxlength;
-    } 
-
-};
-
-//{ Driver Code Starts.
-
-int main() {
-	//code
-	
-	int t;cin>>t;
-	while(t--)
-	{
-	    int n, k;
-	    cin>> n >> k;
-	    int a[n];
-	    
-	    for(int i=0;i<n;i++)
-	        cin>>a[i];
-	   Solution ob;
-	   cout << ob.lenOfLongSubarr(a, n , k)<< endl;
-	    
-	}
-	
-	return 0;
+{\rtf1\ansi\ansicpg1252\cocoartf2638
+\cocoatextscaling0\cocoaplatform0{\fonttbl}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
 }
-// } Driver Code Ends
