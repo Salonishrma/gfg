64d1969c064e4a998655ace60f23a6bbd1541e62
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-05-02 16:53:40 +0530

    Update code 1.cpp

--- solve problems on array(gfg)/Sort an array of 0s, 1s and 2s/code 1.cpp
+++ solve problems on array(gfg)/Sort an array of 0s, 1s and 2s/code 1.cpp
@@ -1,6 +1,47 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // code here 
+        sort(a,a+n);
+        }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
 }
+
+
+// } Driver Code Ends
