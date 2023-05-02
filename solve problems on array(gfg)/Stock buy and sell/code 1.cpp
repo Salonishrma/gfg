54fd7ac810b94cdf23754890d7a1fc078f98f93a
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-05-02 22:46:18 +0530

    Update code 1.cpp

--- solve problems on array(gfg)/Stock buy and sell/code 1.cpp
+++ solve problems on array(gfg)/Stock buy and sell/code 1.cpp
@@ -1,6 +1,54 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+//{ Driver Code Starts
+// Program to find best buying and selling days
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// This function finds the buy sell schedule for maximum profit
+void stockBuySell(int *, int);
+
+// Driver program to test above functions
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n, i;
+        cin >> n;
+        int price[n];
+        for (i = 0; i < n; i++) cin >> price[i];
+        // function call
+        stockBuySell(price, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+// User function template for C++
+
+// This function finds the buy sell schedule for maximum profit
+void stockBuySell(int price[], int n) {
+    // code here
+    int profit=0;
+    int start=0;
+    for(int i=0;i<n-1;i++){
+        if(price[i]<price[i+1] && start==0){
+            cout<<"("<<i<<" ";
+            start=1;
+            profit=1;
+        }
+        else if(price[i]>price[i+1] && start==1){
+             cout<<i<<")"<<" ";
+             start=0;
+        }
+    }
+    if(profit==0){
+        cout<<"No Profit"<<" ";
+    }
+    if(start==1){
+        cout<<n-1<<")"<<" ";
+    }
+    cout<<endl;
 }
