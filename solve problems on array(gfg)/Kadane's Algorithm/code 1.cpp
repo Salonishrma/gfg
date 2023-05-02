d4f2ccc42429504a135352348644e9f39333d07d
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-05-02 20:16:36 +0530

    Update code 1.cpp

--- solve problems on array(gfg)/Kadane's Algorithm/code 1.cpp
+++ solve problems on array(gfg)/Kadane's Algorithm/code 1.cpp
@@ -1,6 +1,51 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+   long long maxi=LONG_MIN;
+   long long  sum=0;
+   for(int i=0;i<n;i++){
+       sum+=arr[i];
+       if(sum>maxi){
+           maxi=sum;
+       }
+       if(sum<0){
+           sum=0;
+       }
+   }
+    return maxi;  
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
 }
+
+// } Driver Code Ends
