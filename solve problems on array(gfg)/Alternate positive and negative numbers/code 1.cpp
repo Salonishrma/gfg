eb0a433cb12bace66f4119d8047142779bf22e46
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-05-03 10:41:26 +0530

    Update code 1.cpp

--- solve problems on array(gfg)/Alternate positive and negative numbers/code 1.cpp
+++ solve problems on array(gfg)/Alternate positive and negative numbers/code 1.cpp
@@ -1,6 +1,72 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	void rearrange(int arr[], int n) {
+	    // code here
+	vector<int>pos;
+	vector<int>neg;
+	for(int i=0;i<n;i++){
+	    if(arr[i]>=0){
+	        pos.push_back(arr[i]);
+	    }
+	    else{
+	        neg.push_back(arr[i]);
+	    }
+	}
+    int p1=pos.size();
+    int n1=neg.size();
+    if(p1<n1){
+        for(int i=0;i<p1;i++){
+            arr[2*i]=pos[i];
+            arr[2*i+1]=neg[i];
+        }
+        int index=p1*2;
+        for(int i=p1;i<n1;i++){
+            arr[index]=neg[i];
+            index++;
+        }
+    }
+    else{
+        for(int i=0;i<n1;i++){
+            arr[2*i]=pos[i];
+            arr[2*i+1]=neg[i];
+        }
+        int index=n1*2;
+        for(int i=n1;i<p1;i++){
+            arr[index]=pos[i];
+            index++;
+        }
+    }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
 }
+
+// } Driver Code Ends
