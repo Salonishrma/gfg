1d26578d5334e674a40280f81603aa75ae49104b
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-05-04 14:56:36 +0530

    Update code.cpp

--- solve problems on array(gfg)/Make Zeros/code.cpp
+++ solve problems on array(gfg)/Make Zeros/code.cpp
@@ -1,6 +1,73 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    void MakeZeros(vector<vector<int> >& matrix) {
+       int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>>temp=matrix;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(temp[i][j]==0)
+                {
+                    if(i+1<n)
+                    {
+                       matrix[i][j]+=temp[i+1][j]; 
+                       matrix[i+1][j]=0;
+                    }
+                     if(i-1>=0)
+                    {
+                       matrix[i][j]+=temp[i-1][j];
+                       matrix[i-1][j]=0;
+                    }
+                    
+                    if(j+1<m)
+                    {
+                       matrix[i][j]+=temp[i][j+1];
+                       matrix[i][j+1]=0;
+                    }
+                     if(j-1>=0)
+                    {
+                       matrix[i][j]+=temp[i][j-1];
+                       matrix[i][j-1]=0;
+                    }
+                }
+            }
+        }
+       
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>> matrix(n, vector<int>(m,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> matrix[i][j];
+			}
+		}
+		Solution ob;
+		ob.MakeZeros(matrix);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++)
+				cout << matrix[i][j] <<" ";
+			cout << "\n";
+		}
+	}
+	return 0;
 }
+// } Driver Code Ends
