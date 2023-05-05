2b54c2d57b28cb85346c16e8bf7e95c9dff26d35
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-05-05 17:45:44 +0530

    Update code 1.cpp

--- solve problems on array(gfg)/Pascal Triangle/code 1.cpp
+++ solve problems on array(gfg)/Pascal Triangle/code 1.cpp
@@ -1,6 +1,60 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+// #define ll long long
+class Solution{
+public:
+   int mod=1e9+7;
+ void solve(int n,vector<ll>&ans){
+     if(n==2){
+         ans[0]=1;
+         ans[1]=1;
+         return;
+     }
+     vector<ll>temp(n-1); //make a row is row k element ko add krke next row k element aynge
+     solve(n-1,temp);
+     ans[0]=1;
+     ans[n-1]=1;
+     for(int i=0;i<n-2;i++){
+         ans[i+1]=(temp[i]+temp[i+1])%mod;
+     }
+ }
+    vector<ll> nthRowOfPascalTriangle(int n) {
+        // code here
+        if(n==1)
+        return{1};
+        vector<ll> ans(n);
+        solve(n,ans);
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+void printAns(vector<ll> &ans) {
+    for (auto &x : ans) {
+        cout << x << " ";
+    }
+    cout << "\n";
 }
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.nthRowOfPascalTriangle(n);
+        printAns(ans);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
